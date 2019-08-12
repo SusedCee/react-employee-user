754ab1dff11605efd9ec0533c2fb32d222384ddb
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-08-11 18:04:20 -0600

    create read update and delete

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EmployeeContainer from './EmployeeContainer';
+// import { Route, Switch } from 'react-router-dom';
+// import Register from './Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <EmployeeContainer/>
+        
+      </div>
+    );
+  }
 }
+
+
+
+// Will have to change code to this at end of project-------------------
+// const My404 = () => {
+//   return (
+//     <div>
+//       You are lost buddy
+//       </div>
+//       )
+// };
+
+// function App() {
+//   return (
+//     <main>
+//         <Switch>
+//           <Route exact path='/' component={Register} />
+//           <Route exact path='/employees' component={ EmployeeContainer} />
+//           <Route component={My404}/>
+//         </Switch>
+//     </main>
+//   );
+// }
 
 export default App;
